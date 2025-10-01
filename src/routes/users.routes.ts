4e2348136ed2a804a5ea8c70fa02d1c7aea2ddbb
Soham Jobanputra <soham.jobanputra@prominentpixel.com>
2025-10-01 12:18:59 +0530

    Login required to access the users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-// import loginRequiredMiddleware from "../common/middleware/loginRequiredMiddleware.ts";
+import loginRequiredMiddleware from "../common/middleware/loginRequiredMiddleware.ts";
 import { getAllUsers } from "../controllers/users.controller.ts";
 
 export const usersRouter = Router();
 
-// usersRouter.use(loginRequiredMiddleware);
+usersRouter.use(loginRequiredMiddleware);
 
 usersRouter.get("/all", getAllUsers);
